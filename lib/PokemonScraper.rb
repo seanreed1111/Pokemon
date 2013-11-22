c0c9f71f3c3e04e0809465e3d838f657e25515dc
Sean Reed <seanreed1111@gmail.com>
2013-11-22 12:27:02 -0500

    adds working PokemonScraper.rb. Will retrieve name of any Pokemon on bulbapedia page

--- lib/PokemonScraper.rb
+++ lib/PokemonScraper.rb
@@ -18,11 +18,29 @@ class PokemonScraper
 		@html = Nokogiri::HTML(download)
 	end
 
-	def get_name
-		html.search("a [href]").text
+
+	def get_h1
+		html.search("h1").text
 	end
+
+	def get_a_href
+		html.search('a[href="/wiki/Wartortle_(Pok%C3%A9mon)"]').text
+	end
+
+	def get_a_pokemon(name)
+		html.search('a[href="/wiki/' + (name.capitalize) + '_(Pok%C3%A9mon)"]').text
+	end
+
 
 
 end
 
-my_scraper = Scraper.new("http://flatironschool-bk.herokuapp.com")
+my_scraper = PokemonScraper.new("http://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number")
+
+puts "my_scraper.get_h1 is #{my_scraper.get_h1}"
+
+puts "my_scraper.get_a_href is #{my_scraper.get_a_href}"
+
+puts "get_a_pokemon(espurr) = #{my_scraper.get_a_pokemon("espurr")}" #works!
+
+
